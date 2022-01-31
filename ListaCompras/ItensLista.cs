c0c21e888916403af4eb1662a2b8ccf7ac3ad1c6
Sup3rPlays <73548496+SPFck@users.noreply.github.com>
2022-01-31 23:05:12 +0000

    Add files via upload

---
+++ ListaCompras/ItensLista.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ListaCompras
+{
+    public class TheBeginningAfterTheEnd : Manga
+    {
+        public TheBeginningAfterTheEnd() : base("The Beginning After The End", "TurtleMe", 980989, 154549129, "Ongoing", 25, "tbaf") { }
+    }
+    public class SoloLeveling : Manga
+    {
+        public SoloLeveling() : base("Solo Leveling", "Sung-Lak Jang", 980474, 333868559, "Ongoing", 25, "sl") { }
+    }
+    public class KimetsuNoYaiba : Manga
+    {
+        public KimetsuNoYaiba() : base("Kimetsu No Yaiba", "Gotouge Koyoharu", 970571, 179071778, "Ongoing", 25, "k") { }
+    }
+    public class ChainsawMan : Manga
+    {
+        public ChainsawMan() : base("Chainsaw Man", "Tatsuki Fujimoto", 980422, 72895428, "Ongoing", 25, "csw") { }
+    }
+    public class JujutsuKaisen : Manga
+    {
+        public JujutsuKaisen() : base("Jujutsu Kaisen", "Akutami Gege", 979135, 67655135, "Ongoing", 25, "jjk") { }
+    }
+    public class BokuNoHeroAcademia : Manga
+    {
+        public BokuNoHeroAcademia() : base("Boku No Hero Academia", "Horikoshi Kouhei", 951973, 125478284, "Ongoing", 25, "bkm") { }
+    }
+    public class TenseiShitaraSlimeDattaKen : Manga
+    {
+        public TenseiShitaraSlimeDattaKen() : base("Tensei Shitara Slime Datta Ken", "Fuse - Kawakami Taiki", 953334, 125804299, "Ongoing", 25, "tst") { }
+    }
+    public class OnePunchMan : Manga
+    {
+        public OnePunchMan() : base("Onepunch-Man", "One - Murata Yuusuke", 951838, 125865622, "Ongoing", 25, "opm") { }
+    }
+    public class Bleach : Manga
+    {
+        public Bleach() : base("Bleach", "Kubo Tite", 951425, 71588867, "Ongoing", 25, "blbl") { }
+    }
+    public class GoblinSlayer : Manga
+    {
+        public GoblinSlayer() : base("Goblin Slayer", "Kagyu Kumo - Kurose Kousuke", 971827, 64648533, "Ongoing", 25, "xas") { }
+    }
+}
---
+++ ListaCompras/Manga.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ListaCompras
+{
+    public class Manga
+    {
+        public string Nome { get; set; }
+        public string Author { get; set; }
+        public int MangaCode { get; set; }
+        public int TotalViews { get; set; }
+        public string CurrentStatus { get; set; }
+        public double Custo { get; set; }
+        public double Preco { get; set; }
+        public string MangaImage { get; set; }
+        public Manga(string n, string a, int m, int t, string c, double p, string mi)
+        {
+            this.Nome = n;
+            this.Author = a;
+            this.MangaCode = m;
+            this.TotalViews = t;
+            this.CurrentStatus = c;
+            this.Preco = p;
+            this.MangaImage = mi;
+        }
+        public void setStatus(string status)
+        {
+            this.CurrentStatus = status;
+        }
+        public string getStatus()
+        {
+            return this.CurrentStatus;
+        }
+        public void resetViews()
+        {
+            this.TotalViews = 0;
+        }
+
+        public void setCusto(double qnt)
+        {
+            this.Custo += this.Preco * qnt;
+        }
+        public double getCustoCompra(double qnt)
+        {
+            return qnt * this.Preco;
+        }
+        public void resetCusto()
+        {
+            this.Custo = 0;
+        }
+    }
+}
